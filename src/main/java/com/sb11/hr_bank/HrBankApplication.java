7bdbf624f5a5a05326bb0154ecbe9e36565470c6
Linpield <top09100620@gmail.com>
2026-04-14 10:23:59 +0900

    feat : init

---
+++ src/main/java/com/sb11/hr_bank/HrBankApplication.java
@@ -0,0 +1,13 @@
+package com.sb11.hr_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sb11/hr_bank/HrBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb11.hr_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
